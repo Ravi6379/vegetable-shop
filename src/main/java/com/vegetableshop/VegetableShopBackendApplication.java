b6108f693dc39a5433803896568d3b2adbe1e21d
Ravi6379 <ravikumar20804@gmail.com>
2026-07-01 14:11:01 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/vegetableshop/VegetableShopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vegetableshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VegetableShopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VegetableShopBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vegetableshop/VegetableShopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vegetableshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VegetableShopBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
